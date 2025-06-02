117e299c1823f0c8a0a7eff72974d521342bf24c
Jairo Navez <jairowolf123@hotmail.com>
2025-06-02 00:24:56 -0500

    Pedido: agregar validación + control de stock

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -24,14 +24,22 @@ class PedidoController extends Controller
         $data_pedido = $request->all();
         $data_pedido['fecha'] = Carbon::now()->toDateTimeString();
         $data_pedido['estado'] = Constantes::PEDIDO_ESTADO_EN_PROCESO;
+
+        foreach ($data_pedido['order_detail'] as $item) {
+            $producto = Producto::find($item['producto_id']);
+            if ($producto->stock < $item['cantidad'])
+                return response()->json(['message' => 'Stock insuficiente del producto ' . $producto->nombre.', solo hay '.$producto->stock.' unidades', 'success' => false], 201);
+        }
+
         DB::beginTransaction();
         $pedido = Pedido::create($data_pedido);
         foreach ($data_pedido['order_detail'] as $item) {
             $item['pedido_id'] = $pedido->id;
             PedidoDetalle::create($item);
+            Producto::where('id', $item['producto_id'])->decrement('stock', $item['cantidad']);
         }
         DB::commit();
-        return response()->json($pedido, 201);
+        return response()->json(['message' => '', 'success' => true], 201);
     }
 
     public function show($id)
